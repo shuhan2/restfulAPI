690480240c8e332f63d1407c8fa8a458dfe41ddb
shuhan <2472520650@qq.com>
2018-05-12 15:52:21 +0800

    init

---
+++ src/test/java/com/example/employee/restfulapi/RestfulApiApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.employee.restfulapi;
+
+import com.example.employee.restfulapi.repository.EmployeeRepository;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RestfulApiApplicationTests {
+//  @Autowired
+//  private EmployeeRepository employeeRepository;
+//  @Before
+//  public void setUp() throws Exception {
+//    //本地启动mysql，创建employee_db数据库
+//    Flyway flyway = new Flyway();
+//    flyway.setDataSource("jdbc:mysql://localhost:3306/employee_db","root","root");
+//    flyway.clean();
+//    flyway.migrate();
+//  }
+
+
+}
