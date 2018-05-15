7683b444481fb9d464db02d749db9079bc0baa74
shuhan <2472520650@qq.com>
2018-05-15 21:29:19 +0800

    findPageEmployees

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.example.employee.restfulapi.repository.EmployeeRepository;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 //import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -17,19 +18,12 @@ import java.util.Optional;
 @RequestMapping(value = "/employees")
 public class EmployeeController {
     //在此处完成Employee API
-
-
-
-//    PUT       /employees/1  #更新某个employeegit
-//    DELETE    /employees/1  #删除某个employee
     @Autowired
     EmployeeRepository employeeRepository;
-
-
     //    GET       /employees    #获取employee列表
     @GetMapping
     List<Employee> getEmployeesList(){return employeeRepository.findAll();}
-//        GET       /employees/1  #获取某个具体employee
+    //    GET       /employees/1  #获取某个具体employee
     @GetMapping(value = "{id}")
     Employee getEmployeeById(@PathVariable long id) throws NotFoundException{
         Employee employee =  employeeRepository.findOne(id);
@@ -41,21 +35,23 @@ public class EmployeeController {
         }
     }
     //    GET       /employees/page/1/pageSize/5  #分页查询，page等于1，pageSize等于5
-//    @GetMapping(value = "page/{page}/pageSize/{pageSize}")
-//    Page<Employee> getEmployeesByPageAndPageSize(@PathVariable int page,@PathVariable int pageSize)throws NotFoundException{
-//        List<Employee> employees= employeeRepository.
-//    }
+    @GetMapping(value = "page/{page}/pageSize/{pageSize}")
+    List<Employee> getEmployeesByPageAndPageSize(@PathVariable int page,@PathVariable int pageSize)throws NotFoundException {
+        Page<Employee> employees = employeeRepository.findAll(new PageRequest(page, pageSize));
+       // return employees.getContent();
+        return employees.getContent();
+    }
     //    GET       /employees/male   #筛选出所有男性Employee
     @GetMapping(value = "male")
     List<Employee> findAllMaleEmployees(){
         return employeeRepository.findByGenderIsIn("male");
     }
-//    POST      /employees    #增加一个employee
+    //   POST      /employees    #增加一个employee
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     Employee saveEmployee(@RequestBody Employee employee){return employeeRepository.save(employee);}
 
-    //    PUT       /employees/1  #更新某个employeegit
+    //   PUT       /employees/1  #更新某个employeegit
     @PutMapping("{id}")
     Employee putEmployee(@PathVariable long id, @RequestBody Employee employee)  throws NotFoundException{
         Employee employee2 =employeeRepository.findById(id);
@@ -64,6 +60,19 @@ public class EmployeeController {
         }
         employee.setId(id);
         return employeeRepository.save(employee);
+    }
+    //    DELETE    /employees/1  #删除某个employee
+    @DeleteMapping("{id}")
+    String deleteEmployee(@PathVariable long id) throws NotFoundException{
+        Employee employee =employeeRepository.findById(id);
+        if (employee==null){
+            throw new NotFoundEx(id,"employee");
+        }
+        else {
+            employeeRepository.delete(employeeRepository.findById(id));
+            return "delete success";
+        }
+
     }
 
 }
