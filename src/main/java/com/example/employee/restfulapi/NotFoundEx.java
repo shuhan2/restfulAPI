9e9a011ceebeea7143f2477fd5945511db6d51b6
shuhan <2472520650@qq.com>
2018-05-15 16:48:22 +0800

    FindEmployeeById(Contains 404)

--- src/main/java/com/example/employee/restfulapi/NotFoundEx.java
+++ src/main/java/com/example/employee/restfulapi/NotFoundEx.java
@@ -1,4 +1,13 @@
 package com.example.employee.restfulapi;
 
-public class NotFoundEx {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundEx extends RuntimeException
+{
+    public NotFoundEx(long id,String name) {
+        super("could not find '" + name +id + "'.");
+
+    }
 }
