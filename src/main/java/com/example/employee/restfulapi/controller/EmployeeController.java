71f96f2c7275d96be88e5c959ce9ed0cea38bf49
shuhan <2472520650@qq.com>
2018-05-15 16:47:37 +0800

    FindEmployeeById(Contains 404)

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 //import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import com.example.employee.restfulapi.NotFoundEx;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,7 +19,7 @@ public class EmployeeController {
     //    GET       /employees/page/1/pageSize/5  #分页查询，page等于1，pageSize等于5
 //    GET       /employees/male   #筛选出所有男性Employee
 //    POST      /employees    #增加一个employee
-//    PUT       /employees/1  #更新某个employee
+//    PUT       /employees/1  #更新某个employeegit
 //    DELETE    /employees/1  #删除某个employee
     @Autowired
     EmployeeRepository employeeRepository;
@@ -29,11 +30,18 @@ public class EmployeeController {
     //    GET       /employees    #获取employee列表
     @GetMapping
     List<Employee> getEmployeesList(){return employeeRepository.findAll();}
-    //    GET       /employees/1  #获取某个具体employee
-//    @GetMapping(value = "{id}")
-//    Employee getEmployee(@PathVariable long id) throws NotFoundException{
-//        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
-//    }
+//        GET       /employees/1  #获取某个具体employee
+    @GetMapping(value = "{id}")
+    Employee getEmployee(@PathVariable long id) throws NotFoundException{
+        Employee employee =  employeeRepository.findOne(id);
+        if(employee!= null) {
+            return employeeRepository.findOne(id);
+        }
+        else {
+            throw new NotFoundEx(id,"employee");
+        }
+    }
+
 
 
 }
